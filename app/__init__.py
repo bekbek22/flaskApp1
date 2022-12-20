d227e1e7b34f560e544a9435949c087bf8ac4a90
bekbek22 <bekbeknakub@gmail.com>
2022-12-20 11:55:27 +0700

    Add a New Message

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,6 @@ from flask import Flask
 app = Flask(__name__, static_folder='static')
  
 app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = '44eb67b04da4c223a6b6529533cf0b21fbd65e6bee4781a9'
  
 from app import views  # noqa
