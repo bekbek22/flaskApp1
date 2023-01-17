c059246f3bdaea462da809e0a5bc2b8177bc0a9e
bekbek22 <bekbeknakub@gmail.com>
2023-01-17 11:13:26 +0700

    AJAX

--- app/__init__.py
+++ app/__init__.py
@@ -4,5 +4,6 @@ app = Flask(__name__, static_folder='static')
  
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = '44eb67b04da4c223a6b6529533cf0b21fbd65e6bee4781a9'
- 
+app.config['JSON_AS_ASCII'] = False
+
 from app import views  # noqa
